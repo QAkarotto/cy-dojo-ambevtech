7f9e441e2c524593ec74214fc29cffdb7b02d8c4
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-08-31 15:22:12 -0300

    test(semana3): indentação

--- cypress/e2e/3-ui/cucumber/login-cucumber.cy.js
+++ cypress/e2e/3-ui/cucumber/login-cucumber.cy.js
@@ -1,23 +1,17 @@
-describe('Funcionalidade: Login', () => {
-    context('Dado que eu esteja na página de Login', () => {
-        before(() => {
-            cy.visit('login')
-        });
+describe("Funcionalidade: Login", () => {
+	context("Dado que eu esteja na página de Login", () => {
+		before(() => {
+			cy.visit("login");
+		});
 
-        context('Quando eu inserir usuário e senha', () => {
-            beforeEach(() => {
-                cy.login('goku7@email.com', 'senha@123')
-            });
-            it('Então deve exibir a mensagem de boas vindas no dashboard', () => {
+		context("Quando eu inserir usuário e senha", () => {
+			beforeEach(() => {
+				cy.login("goku7@email.com", "senha@123");
+			});
 
-
-                cy.get('[data-test="dashboard-welcome"]').should('contain', 'Bem-vindo')
-            });
-
-
-
-        });
-
-    });
-
+			it("Então deve exibir a mensagem de boas vindas no dashboard", () => {
+				cy.get('[data-test="dashboard-welcome"]').should("contain", "Bem-vindo");
+			});
+		});
+	});
 });
