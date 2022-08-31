789b1daa751b9cc020569dd1af3fb3dc4cf435c9
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-08-31 17:43:36 -0300

    test(semana4): configuração Cypress Dashboard

--- cypress/e2e/3-ui/profile.cy.js
+++ cypress/e2e/3-ui/profile.cy.js
@@ -1,32 +1,37 @@
 /// <reference types="cypress" />
-import mockProfile from "../../fixtures/profile.json"
+import mockProfile from "../../fixtures/perfis.json";
 
-describe('Funcionalidade: Visualizar Perfil', () => {
+describe("Funcionalidade: Visualizar Perfil", () => {
+	beforeEach(() => {
+		cy.intercept(
+			{
+				method: "GET",
+				url: "api/profile",
+			},
+			{
+				statusCode: 200,
+				body: mockProfile,
+			}
+		);
 
-    beforeEach(() => {
-        cy.intercept({
-            method: 'GET',
-            url: 'api/profile'
-        }, {
-         statusCode: 200, 
-         body: mockProfile  
-        })
+		cy.visit("perfis");
+	});
 
-        cy.visit('perfis')
-    });
+	it.only("Deve validar o primeiro item da lista com sucesso", () => {
+		cy.get('[data-test="profile-name"]')
+			.first()
+			.should("have.text", "Goku Guerra");
+	});
 
-    it.only('Deve validar o primeiro item da lista com sucesso', () => {
-        
+	it("Deve validar o último item da lista com sucesso", () => {
+		cy.get('[data-test="profile-name"]')
+			.last()
+			.should("have.text", "Goku dos Santos Filho");
+	});
 
-        cy.get('[data-test="profile-name"]').first().should('have.text', 'Goku Guerra')
-    });
-
-    it('Deve validar o último item da lista com sucesso', () => {
-        cy.get('[data-test="profile-name"]').last().should('have.text', 'Goku dos Santos Filho')
-    });
-
-    it('Deve validar o 4º item da lista com sucesso', () => {
-        cy.get('[data-test="profile-name"]').eq(3).should('have.text', 'Goku Santos Silva')
-    });
-
+	it("Deve validar o 4º item da lista com sucesso", () => {
+		cy.get('[data-test="profile-name"]')
+			.eq(3)
+			.should("have.text", "Goku Santos Silva");
+	});
 });
