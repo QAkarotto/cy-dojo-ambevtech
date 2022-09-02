87c1e421c9f91eb41de258e28cf7da7029d20d7d
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-09-02 17:27:15 -0300

    test(semana4): configurando Allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
 const { defineConfig } = require("cypress");
+const AllureWriter  = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  projectId: "c5gk8u",
-  video: false,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'https://conexaoqa.herokuapp.com/'
-  },
+	projectId: "c5gk8u",
+	video: false,
+	e2e: {
+		setupNodeEvents(on, config) {
+			AllureWriter(on, config);
+
+      const version = config.env.version || 'prd'
+      config.env = require(`./cypress/env/${version}.json`)
+      config.baseUrl = config.env.baseUrl
+
+			return config;
+		},
+		baseUrl: "https://conexaoqa.herokuapp.com/",
+	},
 });
