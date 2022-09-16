a3b06c71179957e484f9cede6e62738eb36b736a
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-09-16 17:46:02 -0300

    wip(desafio-semana3): acessando tela de perfil

--- cypress/e2e/3-ui/e2e-conexao-qa.cy.js
+++ cypress/e2e/3-ui/e2e-conexao-qa.cy.js
@@ -1,3 +1,5 @@
+const faker = require('faker-br');
+
 describe('Testes e2e em conexão QA', () => {
     it('Deve fazer o fluxo completo de criação de perfil completo ponta a ponta', () => {
         const nomeFake = 'Goku ' + faker.name.lastName();
@@ -5,6 +7,9 @@ describe('Testes e2e em conexão QA', () => {
         const senhaFake = faker.internet.password();
 
         cy.cadastrar(nomeFake, emailFake, senhaFake, senhaFake);
+
+        cy.get('[data-test="dashboard-createProfile"]').click()
+        cy.get('#mui-component-select-status').select('QA Pleno')
 
         
     });
