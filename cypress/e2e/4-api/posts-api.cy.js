a4b426d431ede581db353426f43d70efff4634f3
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-09-16 17:30:18 -0300

    test(posts-api): testes da funcionalidade de posts via API

--- cypress/e2e/4-api/posts-api.cy.js
+++ cypress/e2e/4-api/posts-api.cy.js
@@ -1,128 +1,99 @@
-describe('Funcionalidade: Posts via API', () => {
+describe("Funcionalidade: Posts via API", () => {
+	let token;
+	let postCriadoId;
+	let comentarioCriado;
 
-    let token
-    let postCriado
+	beforeEach(() => {
+		cy.gerarTokenAPI("goku@email.com", "senha@123").then((tkn) => {
+			token = tkn;
+		});
 
-    beforeEach(() => {
-        cy.gerarToken("goku@email.com", "senha@123").then((tkn) => {
-            token = tkn
-        })
+		cy.criarPostAPI(token, "Post Criado").then((response) => {
+			postCriadoId = response.body._id;
+		});
+	});
 
-        cy.criarPostAPI(token, "test").then((idpost) => {
-            postCriado = idpost
-        })
+	it("GET Deve consultar todos os posts com sucesso", () => {
+		cy.consultarPostsAPI(token).then((response) => {
+			expect(response.status).eq(200);
+			expect(response.body).not.be.null;
+			expect(response.body).to.be.a("array");
+		});
+	});
 
-    });
+	it("POST Deve inserir um novo post com sucesso", () => {
+		cy.criarPostAPI(token, "Novo Post").then((response) => {
+			expect(response.status).eq(201);
+			expect(response.body._id).not.be.null;
+			expect(response.body.text).eq("Novo Post");
+			expect(response.body.name).not.be.null;
+		});
+	});
 
-    // beforeEach(() => {
-    //     cy.gerarToken("goku@email.com", "senha@123").then((tkn) => {
-    //         token = tkn
-    //     })
-    // });
+	it("GET Deve consultar um post com sucesso", () => {
+		cy.consultarUmPostAPI(token, postCriadoId).then((response) => {
+			expect(response.status).eq(200);
+			expect(response.body._id).not.be.null;
+			expect(response.body.text).eq("Post Criado");
+			expect(response.body.name).not.be.null;
+		});
+	});
 
-    it('GET Deve consultar todos os posts', () => {
-        cy.request({
-            method: 'GET',
-            url: '/api/posts',
-            headers: {
-                Cookie: token
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(200)
-            expect(response.body).not.be.null
-            expect(response.body).to.be.a('array')
+	it("DELETE Deve deletar um post com sucesso", () => {
+		cy.deletarPostAPI(token, postCriadoId).then((response) => {
+			expect(response.status).eq(200);
+			expect(response.body.msg).eq("Post removido");
+		});
+	});
 
-        })
+	it("PUT Deve curtir um post com sucesso", () => {
+		cy.curtirPostAPI(token, postCriadoId).then((response) => {
+			expect(response.status).eq(200);
+			expect(response.body[0].user).not.be.null;
+			expect(response.body[0]._id).not.be.null;
+		});
+	});
 
-    });
+	it("PUT Deve descurtir um post com sucesso", () => {
+		cy.curtirPostAPI(token, postCriadoId).then(() => {
+			cy.descurtirPostAPI(token, postCriadoId).then((response) => {
+				expect(response.status).eq(200);
+			});
+		});
+	});
 
-    it('POST Deve inserir um novo post', () => {
+	it("POST Deve inserir um novo comentário em um post com sucesso", () => {
+		cy.comentarPostAPI(
+			token,
+			postCriadoId,
+			"Teste comentário criado com sucesso"
+		)
+			.then((commentId) => {
+				comentarioCriado = commentId;
+			})
+			.then((response) => {
+				expect(response.status).eq(201);
+				expect(response.body[0]._id).not.be.null;
+				expect(response.body[0].text).eq(
+					"Teste comentário criado com sucesso"
+				);
+				expect(response.body[0].name).eq("Goku");
+			});
+	});
 
-        cy.request({
-            method: 'POST',
-            url: '/api/posts',
-            headers: {
-                Cookie: token
-            },
-            body: {
-                "text": "Oi eu sou o Goku"
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(201)
-            expect(response.body._id).not.be.null
-            expect(response.body.text).eq("Oi eu sou o Goku")
-            expect(response.body.name).eq("Goku")
-
-        })
-    });
-
-    it('GET Deve consultar um post', () => {
-
-        cy.request({
-            method: 'GET',
-            url: `/api/posts/${postCriado}`,
-            headers: {
-                Cookie: token
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(200)
-            expect(response.body._id).not.be.null
-            expect(response.body.text).eq("test")
-            expect(response.body.name).eq("Goku")
-
-        })
-    });
-
-    it('DELETE Deve deletar um post', () => {
-
-        cy.request({
-            method: 'DELETE',
-            url: `/api/posts/${postCriado}`,
-            headers: {
-                Cookie: token
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(200)
-            expect(response.body.msg).eq("Post removido")
-        })
-    });
-
-    it('PUT Deve adicionar like a um post', () => {
-
-        cy.request({
-            method: 'PUT',
-            url: `/api/posts/like/${postCriado}`,
-            headers: {
-                Cookie: token
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(200)
-            expect(response.body[0].user).eq("62ed60eeaac3800015ece7b6")
-            expect(response.body[0]._id).not.be.null
-
-        })
-    });
-
-    it('PUT Deve adicionar unlike a um post', () => {
-
-        cy.request({
-            method: 'PUT',
-            url: `/api/posts/unlike/${postCriado}`,
-            headers: {
-                Cookie: token
-            }
-        }).then((response) => {
-            cy.log(response)
-            expect(response.status).eq(200)
-            expect(response.body[0].user).eq("62ed60eeaac3800015ece7b6")
-            expect(response.body[0]._id).not.be.null
-
-        })
-    });
-
+	it("DELETE Deve remover um comentário de um post com sucesso", () => {
+		cy.comentarPostAPI(token, postCriadoId, "Teste deleção de comentário")
+			.then((response) => {
+				comentarioCriado = response.body[0]._id;
+			})
+			.then(() => {
+				cy.deletarComentarioDePostAPI(
+					token,
+					postCriadoId,
+					comentarioCriado
+				).then((response) => {
+					expect(response.status).eq(200);
+				});
+			});
+	});
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,89 +1,138 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('login', (email, password) => {
-    cy.visit('login')
+//UI
+Cypress.Commands.add("login", (email, password) => {
+	cy.visit("login");
 
-    cy.get('[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
-    cy.get('[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input').type(password)
-    cy.get('[data-test="login-submit"]').click()
-})
+	cy.get(
+		'[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(email);
+	cy.get(
+		'[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(password);
+	cy.get('[data-test="login-submit"]').click();
+});
 
-Cypress.Commands.add('gerarToken', (email, password) => {
-    cy.request({
-        method: 'POST',
-        url: '/api/auth',
-        body: {
-            "email": email,
-            "password": password
-        }
-    }).then((response) => {
-        return response.body.jwt
-    })
-})
+Cypress.Commands.add("logout", () => {
+	cy.get('[data-test="navbar-logout"]').click();
+});
 
-Cypress.Commands.add('logout', () => {
-    cy.get('[data-test="navbar-logout"]').click()
-})
+Cypress.Commands.add("cadastrar", (name, email, password, confirmPassword) => {
+	cy.visit("cadastrar");
 
-Cypress.Commands.add('cadastrar', (name, email, password, confirmPassword) => {
-    cy.visit('cadastrar')
+	cy.get(
+		'[data-test="register-name"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(name);
+	cy.get(
+		'[data-test="register-email"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(email);
+	cy.get(
+		'[data-test="register-password"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(password);
+	cy.get(
+		'[data-test="register-password2"] > .MuiInputBase-root > .MuiInputBase-input'
+	).type(confirmPassword);
+	cy.get('[data-test="register-submit"]').click();
+});
 
-    cy.get('[data-test="register-name"] > .MuiInputBase-root > .MuiInputBase-input').type(name)
-    cy.get('[data-test="register-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
-    cy.get('[data-test="register-password"] > .MuiInputBase-root > .MuiInputBase-input').type(password)
-    cy.get('[data-test="register-password2"] > .MuiInputBase-root > .MuiInputBase-input').type(confirmPassword)
-    cy.get('[data-test="register-submit"]').click()
-})
+//API
 
-Cypress.Commands.add('criarPostAPI', (token, text) => {
-    cy.request({
-        method: 'POST',
-        url: '/api/posts',
-        headers: {
-            Cookie: token
-        },
-        body: {
-            "text": text
-        }
-    }).then((response) => {
-        return response.body._id
-    })
-})
+Cypress.Commands.add("gerarTokenAPI", (email, password) => {
+	cy.request({
+		method: "POST",
+		url: "/api/auth",
+		body: {
+			email: email,
+			password: password,
+		},
+	}).then((response) => {
+		return response.body.jwt;
+	});
+});
 
-Cypress.Commands.add('curtirPostAPI', (token, text) => {
-    cy.request({
-        method: 'POST',
-        url: '/api/posts',
-        headers: {
-            Cookie: token
-        },
-        body: {
-            "text": text
-        }
-    }).then((response) => {
-        return response.body._id
-    })
-})
+Cypress.Commands.add("criarPostAPI", (token, text) => {
+	cy.request({
+		method: "POST",
+		url: "/api/posts",
+		headers: {
+			Cookie: token,
+		},
+		body: {
+			text: text,
+		},
+	})
+});
+
+Cypress.Commands.add("consultarPostsAPI", (token) => {
+	cy.request({
+		method: "GET",
+		url: "/api/posts",
+		headers: {
+			Cookie: token,
+		},
+	});
+});
+
+Cypress.Commands.add("consultarUmPostAPI", (token, postId) => {
+	cy.request({
+		method: "GET",
+		url: `/api/posts/${postId}`,
+		headers: {
+			Cookie: token,
+		},
+	});
+});
+
+Cypress.Commands.add("deletarPostAPI", (token, postId) => {
+	cy.request({
+		method: "DELETE",
+		url: `/api/posts/${postId}`,
+		headers: {
+			Cookie: token,
+		},
+	});
+});
+
+Cypress.Commands.add("curtirPostAPI", (token, postId) => {
+	cy.request({
+		method: "PUT",
+		url: `/api/posts/like/${postId}`,
+		headers: {
+			Cookie: token,
+		},
+	});
+});
+
+Cypress.Commands.add("descurtirPostAPI", (token, postId) => {
+	cy.request({
+		method: "PUT",
+		url: `/api/posts/unlike/${postId}`,
+		headers: {
+			Cookie: token,
+		},
+	});
+});
+
+Cypress.Commands.add("comentarPostAPI", (token, postId, text) => {
+	cy.request({
+		method: "POST",
+		url: `/api/posts/comment/${postId}`,
+		headers: {
+			Cookie: token,
+		},
+		body: {
+			text: text,
+		},
+	});
+});
+
+Cypress.Commands.add(
+	"deletarComentarioDePostAPI",
+	(token, postId, commentId) => {
+		cy.request({
+			method: "DELETE",
+			url: `/api/posts/comment/${postId}/${commentId}`,
+			headers: {
+				Cookie: token,
+			},
+		});
+	}
+);
