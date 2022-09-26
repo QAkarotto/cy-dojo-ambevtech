f17104232170fbebda98dfac24a7ea4d8203f1bc
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-09-26 11:42:00 -0300

    wip(desafio-semana3): criação de perfil

--- cypress/e2e/3-ui/e2e-conexao-qa.cy.js
+++ cypress/e2e/3-ui/e2e-conexao-qa.cy.js
@@ -1,16 +1,29 @@
-const faker = require('faker-br');
+const faker = require("faker-br");
 
-describe('Testes e2e em conexão QA', () => {
-    it('Deve fazer o fluxo completo de criação de perfil completo ponta a ponta', () => {
-        const nomeFake = 'Goku ' + faker.name.lastName();
-        const emailFake = faker.internet.email(nomeFake);
-        const senhaFake = faker.internet.password();
+describe("Testes e2e em conexão QA", () => {
+	it("Deve fazer o fluxo completo de criação de perfil de ponta a ponta", () => {
+		const nomeFake = "Goku " + faker.name.lastName();
+		const emailFake = faker.internet.email(nomeFake);
+		const senhaFake = faker.internet.password();
+		const perfilFake = {
+			status: "QA Pleno",
+			empresa: faker.company.companyName(),
+			website: faker.internet.url(),
+			localizacao: faker.address.city() + "," + faker.address.state(),
+			conhecimentos: faker.name.jobDescriptor(),
+			usuarioGitHub: faker.internet.userName(),
+			bio: faker.lorem.paragraph(),
+			twitterUrl: faker.internet.url(),
+			facebookUrl: faker.internet.url(),
+			youtubeUrl: faker.internet.url(),
+			linkedinUrl: faker.internet.url(),
+			instagramUrl: faker.internet.url(),
+			mediumUrl: faker.internet.url(),
+		};
 
-        cy.cadastrar(nomeFake, emailFake, senhaFake, senhaFake);
+		cy.cadastrar(nomeFake, emailFake, senhaFake, senhaFake);
 
-        cy.get('[data-test="dashboard-createProfile"]').click()
-        cy.get('#mui-component-select-status').select('QA Pleno')
-
-        
-    });
+		cy.criarPerfil(perfilFake);
+        cy.adicionarExperiencia();
+	});
 });
